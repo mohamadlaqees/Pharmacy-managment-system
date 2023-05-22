f7821b51ced9c3edd3e2aae9903398cd7d0143a3
mohamadlaqees <mohamadlk24@gmail.com>
2023-05-22 19:52:34 +0300

    Changed

--- pms/src/pages/Content.js
+++ pms/src/pages/Content.js
@@ -4,7 +4,86 @@ import Header from "../Components/Header";
 function Content() {
   return (
     <Fragment>
-      <div className="page">content </div>
+      <div className="page"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, quae
+      sed! Qui aliquam architecto animi. Blanditiis provident, eveniet tenetur
+      cumque maiores excepturi, unde veritatis aspernatur sapiente eligendi
+      laboriosam. Nisi, ut! Lorem ipsum dolor sit, amet consectetur adipisicing
+      elit. Vel, error laudantium amet, minus quisquam aut omnis distinctio
+      dolorem nisi ex deleniti dignissimos? Omnis non sunt consequuntur facere
+      odit quis tenetur! Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Odit ipsum iure laborum temporibus dolorum tempore esse, animi
+      consequuntur perferendis praesentium minima voluptatem corrupti nihil
+      recusandae enim atque. Deserunt, nulla ad? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Odit ipsum iure laborum temporibus dolorum
+      tempore esse, animi consequuntur perferendis praesentium minima voluptatem
+      corrupti nihil recusandae enim atque. Deserunt, nulla ad? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Odit ipsum iure laborum
+      temporibus dolorum tempore esse, animi consequuntur perferendis
+      praesentium minima voluptatem corrupti nihil recusandae enim atque.
+      Deserunt, nulla ad? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Odit ipsum iure laborum temporibus dolorum tempore esse, animi
+      consequuntur perferendis praesentium minima voluptatem corrupti nihil
+      recusandae enim atque. Deserunt, nulla ad? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Odit ipsum iure laborum temporibus dolorum
+      tempore esse, animi consequuntur perferendis praesentium minima voluptatem
+      corrupti nihil recusandae enim atque. Deserunt, nulla ad? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Odit ipsum iure laborum
+      temporibus dolorum tempore esse, animi consequuntur perferendis
+      praesentium minima voluptatem corrupti nihil recusandae enim atque.
+      Deserunt, nulla ad? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Odit ipsum iure laborum temporibus dolorum tempore esse, animi
+      consequuntur perferendis praesentium minima voluptatem corrupti nihil
+      recusandae enim atque. Deserunt, nulla ad? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Odit ipsum iure laborum temporibus dolorum
+      tempore esse, animi consequuntur perferendis praesentium minima voluptatem
+      corrupti nihil recusandae enim atque. Deserunt, nulla ad? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Odit ipsum iure laborum
+      temporibus dolorum tempore esse, animi consequuntur perferendis
+      praesentium minima voluptatem corrupti nihil recusandae enim atque.
+      Deserunt, nulla ad? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Odit ipsum iure laborum temporibus dolorum tempore esse, animi
+      consequuntur perferendis praesentium minima voluptatem corrupti nihil
+      recusandae enim atque. Deserunt, nulla ad? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Odit ipsum iure laborum temporibus dolorum
+      tempore esse, animi consequuntur perferendis praesentium minima voluptatem
+      corrupti nihil recusandae enim atque. Deserunt, nulla ad? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Odit ipsum iure laborum
+      temporibus dolorum tempore esse, animi consequuntur perferendis
+      praesentium minima voluptatem corrupti nihil recusandae enim atque.
+      Deserunt, nulla ad? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Odit ipsum iure laborum temporibus dolorum tempore esse, animi
+      consequuntur perferendis praesentium minima voluptatem corrupti nihil
+      recusandae enim atque. Deserunt, nulla ad? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Odit ipsum iure laborum temporibus dolorum
+      tempore esse, animi consequuntur perferendis praesentium minima voluptatem
+      corrupti nihil recusandae enim atque. Deserunt, nulla ad? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Odit ipsum iure laborum
+      temporibus dolorum tempore esse, animi consequuntur perferendis
+      praesentium minima voluptatem corrupti nihil recusandae enim atque.
+      Deserunt, nulla ad? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Odit ipsum iure laborum temporibus dolorum tempore esse, animi
+      consequuntur perferendis praesentium minima voluptatem corrupti nihil
+      recusandae enim atque. Deserunt, nulla ad? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Odit ipsum iure laborum temporibus dolorum
+      tempore esse, animi consequuntur perferendis praesentium minima voluptatem
+      corrupti nihil recusandae enim atque. Deserunt, nulla ad? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Odit ipsum iure laborum
+      temporibus dolorum tempore esse, animi consequuntur perferendis
+      praesentium minima voluptatem corrupti nihil recusandae enim atque.
+      Deserunt, nulla ad? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Odit ipsum iure laborum temporibus dolorum tempore esse, animi
+      consequuntur perferendis praesentium minima voluptatem corrupti nihil
+      recusandae enim atque. Deserunt, nulla ad? Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Odit ipsum iure laborum temporibus dolorum
+      tempore esse, animi consequuntur perferendis praesentium minima voluptatem
+      corrupti nihil recusandae enim atque. Deserunt, nulla ad? Lorem ipsum
+      dolor sit amet consectetur adipisicing elit. Odit ipsum iure laborum
+      temporibus dolorum tempore esse, animi consequuntur perferendis
+      praesentium minima voluptatem corrupti nihil recusandae enim atque.
+      Deserunt, nulla ad? Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Odit ipsum iure laborum temporibus dolorum tempore esse, animi
+      consequuntur perferendis praesentium minima voluptatem corrupti nihil
+      recusandae enim atque. Deserunt, nulla ad?</div>
     </Fragment>
   );
 }
