27bd6ab52e180fa4637676bde801a36c603c66e8
mohamadlaqees <mohamadlk24@gmail.com>
2023-06-06 20:18:41 +0300

    .

--- pms/src/pages/Employees.js
+++ pms/src/pages/Employees.js
@@ -1,12 +1,193 @@
 import React from "react";
 
 function Employees() {
+  let map = [
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+    {
+      name: "bla",
+      price: "bla",
+      img: "/images/med.jpg",
+      brand: "brand",
+    },
+  ];
   return (
-    <div  className="page">
-     Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci numquam
-      consequuntur consectetur qui minus, ratione omnis, odit, accusamus quae
-      aspernatur dolorem tempore illum! Nobis labore, similique saepe aut
-      corporis inventore!
+    <div className="page ">
+      <div className="flex gap-4">
+        <div className="bg-white pt-4 pb-4 shadow-md rounded-md w-1/2 relative">
+          <div className="absolute top-0 w-full p-1  bg-blue-600 text-white font-bold text-center rounded-md">
+            Employees
+          </div>
+          <div className="absolute bottom-0 w-full h-7  bg-blue-600 text-white font-bold text-center rounded-md"></div>
+          {map.map((e, inx) => {
+            return (
+              <div
+                className={`p-2 ${
+                  inx === map.length - 1 ? "border-b-0 mb-1" : "border-b-2"
+                } border-gray-200 ${
+                  inx === 0 ? "mt-2" : ""
+                } flex gap-3 cursor-pointer hover:bg-slate-200 transition-all`}
+                key={inx}
+              >
+                <div>
+                  <img
+                    src="/images/user.jpg"
+                    alt=""
+                    className="rounded-full w-20 h-20"
+                  />
+                </div>
+                <div className="flex justify-center items-center">
+                  <span className="text-font2">{e.name}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
