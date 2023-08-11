3391ecbfee0f013228b78e90cc7b2442ecb5a34a
Elyas <hamedahty2000@gmail.com>
2023-08-11 23:59:42 +0200

    finished  dahboard overview

--- pms/src/pages/Content.js
+++ pms/src/pages/Content.js
@@ -1,45 +1,113 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import BestSelling from "../Components/BestSelling";
 import StatCard from "../Components/StatCard";
 import StatTile from "../Components/StatTile";
 import { useState } from "react";
 import StatChart from "../Components/StatChart";
+import TopPrducts from "../Components/TopProducts";
+import axios from "../axios";
 
+async function fetchBestSelling() {
+  try {
+    const resp = await axios.get("/dashboard/best-selling?days=1");
+    return resp.data.data;
+  } catch (error) {
+    return error.message;
+  }
+}
+async function fetchMostProfitable() {
+  try {
+    const resp = await axios.get("/dashboard/most-profitable?days=1");
+    return resp.data.data;
+  } catch (error) {
+    return error.message;
+  }
+}
+async function fetchOrderCount() {
+  try {
+  const resp = await axios.get("/dashboard/orders-count?days=1");
+    return resp.data.data;
+  } catch (error) {
+    return error.message;
+  }  
+}
+async function fetchRevenue() {
+  try {
+  const resp = await axios.get("/dashboard/revenue?days=1");
+      return resp.data.data;
+  } catch (error) {
+    return error.message;
+  }
+}
 function Content() {
+  const [BestSelling, setBestSelling] = useState([]);
+  const [MostProfitable, setMostProfitable] = useState([]);
+  const [OrderCount, setOrderCount] = useState(0);
+  const [Revenue, setRevenue] = useState(0);
 
+  useEffect(() => {
+    // Fetch best selling data and update stat
+    fetchBestSelling()
+      .then((data) => setBestSelling(data))
+      .catch((error) =>
+        console.error("Error fetching best selling data:", error)
+      );
+
+    // Fetch most profitable data and update state
+    fetchMostProfitable()
+      .then((data) => setMostProfitable(data))
+      .catch((error) =>
+        console.error("Error fetching most profitable data:", error)
+      );
+
+    // Fetch order count and update state
+    fetchOrderCount()
+      .then((count) => setOrderCount(count))
+      .catch((error) => console.error("Error fetching order count:", error));
+
+    // Fetch revenue data and update state
+    fetchRevenue()
+      .then((revenueData) => setRevenue(revenueData))
+      .catch((error) => console.error("Error fetching revenue data:", error));
+  }, []);
 
   return (
     <>
-      <div className="page pb-0">
-        <Container fluid>
-          <Row>
-            <Col md={4} sm={6} className="mb-1">
-              <StatCard icon="fa-solid fa-list" title="Orders" data="234" />
+      <div className="page pb-0  ">
+        <Container fluid className="">
+          <Row className="">
+            <Col md={4} sm={6} className="mb-1 h-25">
+              <StatCard icon="fa-solid fa-list" title="Orders" data={OrderCount} />
             </Col>
-            <Col md={4} xs={12} sm={6} className="mb-1">
+            <Col md={4} xs={12} sm={6} className="mb-1 h-25">
               <StatCard
                 icon="fas fa-hand-holding-usd"
-                title="Income"
-                data="$50000"
+                title="Revenue"
+                data={`$ ${Revenue}`}
               />
             </Col>
             <Col
               md={4}
               sm={12}
-              className="mb-1 d-flex flex-column justify-content-around"
+              style={{ height: "25vh" }}
+              className="text-center "
             >
-              <StatTile icon="fa fa-line-chart" title="Revenue" data="$50000" />
-              <StatTile
-                icon="fa-solid fa-users"
-                title="New Customers"
-                data="152"
-              />
+              {
+                 <TopPrducts title=" most profitable products" data={MostProfitable} />
+              }{" "}
             </Col>
+            {/*
+          <StatTile icon="fa fa-line-chart" title="Revenue" data="$50000" />
+          <StatTile
+            icon="fa-solid fa-users"
+            title="New Customers"
+            data="152"
+          />
+          */}
           </Row>
           <Row className="mt-3">
             <Col md={8} sm={12} style={{ height: "60vh" }}>
-            <StatChart />
+              <StatChart />
             </Col>
             <Col
               md={4}
@@ -47,7 +115,7 @@ function Content() {
               style={{ height: "60vh" }}
               className="text-center "
             >
-              <BestSelling />
+              <TopPrducts title="Bestselling" data={BestSelling} />
             </Col>
           </Row>
         </Container>
