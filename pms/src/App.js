725797ce69a6e1c2e0676680ca0541f46996d5f9
mohamadlaqees <mohamadlk24@gmail.com>
2023-08-14 00:15:16 +0300

    .

--- pms/src/App.js
+++ pms/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Profile from "./pages/Profile";
 import Product from "./pages/Product";
 import Reports from "./pages/Reports";
