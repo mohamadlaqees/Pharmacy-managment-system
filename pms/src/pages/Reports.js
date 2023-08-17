b132464fac39bdb14ec1e3d0bec203331c368ede
mohamadlaqees <mohamadlk24@gmail.com>
2023-08-17 21:41:54 +0300

    .

--- pms/src/pages/Reports.js
+++ pms/src/pages/Reports.js
@@ -5,6 +5,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { getPurchases } from "../states/supplySlice";
 import AllReports from "../Components/AllReports";
 import Loading from "../Components/loading";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import dayjs from "dayjs";
+import { message } from "antd";
 
 function Reports() {
   const { purchases, total, loadingP } = useSelector(
@@ -13,19 +19,68 @@ function Reports() {
 
   const dispatch = useDispatch();
   const [PN, setPN] = useState(1);
+  const [date, setDate] = useState("");
+
+  const todayDate = new Date();
+  let day = todayDate.getDate();
+  let month = todayDate.getMonth() + 1;
+  let year = todayDate.getFullYear();
+
+  const msg = (type, msg) => {
+    switch (type) {
+      case "success":
+        message.success(msg);
+        break;
+      case "error":
+        message.error(msg);
+        break;
+      default:
+        return "";
+    }
+  };
 
   useEffect(() => {
-    dispatch(getPurchases(PN));
+    dispatch(getPurchases({ PN, date }));
   }, [dispatch, PN]);
 
   const dataInPage = 15;
   const [currentPage, setCurrentPage] = useState(1);
 
+  const dateHandler = (e) => {
+    setDate(dayjs(e.$d).format("YYYY-MM-DD").toString());
+    if (
+      dayjs(e.$d).format("YYYY") <= year &&
+      dayjs(e.$d).format("MM") <= month &&
+      dayjs(e.$d).format("DD") <= day
+    ) {
+      setTimeout(() => {
+        dispatch(
+          getPurchases({
+            PN,
+            date: dayjs(e.$d).format("YYYY-MM-DD").toString(),
+          })
+        );
+      }, 500);
+    } else {
+      msg("error", "The date field is wrong");
+    }
+  };
+
   return (
     <div>
       <Loading loading={loadingP}>
         <div className="bg-white rounded-md p-8 w-full">
-          <div className="mt-8">
+          <div className="flex justify-end">
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DemoContainer components={["DatePicker"]}>
+                <DatePicker
+                  label="Filter by date"
+                  onChange={(e) => dateHandler(e)}
+                />
+              </DemoContainer>
+            </LocalizationProvider>
+          </div>
+          <div className="mt-6">
             <Table striped bordered hover size="sm">
               <thead>
                 <tr className="text-center ">
@@ -33,6 +88,7 @@ function Reports() {
                   <th className="p-4">Quantity</th>
                   <th className="p-4">Total</th>
                   <th className="p-4">Date</th>
+                  <th className="p-4"></th>
                 </tr>
               </thead>
               <AllReports data={purchases} />
